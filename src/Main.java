8fc95a3ed545dd949226c4d3afb8fe0e9c1a99ea
Chokokito <miguelferocha@outlook.com>
2024-07-24 20:57:18 -0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import Exceptions.ParametrosInvalidosException;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Insira um parâmetro válido.");
+        }
+
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int contador = 1; contador <= contagem; contador++) {
+            System.out.println("Imprimindo o número " + contador);
+        }
+    }
+}
